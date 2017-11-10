82dd725d7f853b7808a8d8c042badd351e47c266
Maxwell Bernstein <tekknolagi@gmail.com>
2017-11-09 21:07:20 -0500

    Handle EOF, finally

--- main.ml
+++ main.ml
@@ -2,12 +2,16 @@ let _ =
   try
     let lexbuf = Lexing.from_channel stdin in
     while true do
-      let basis = [] in
-      let ast = L01parser.main L00lexer.token lexbuf in
-      let tast = L02ast.typecheck basis ast in
-      let res = L03eval.eval basis tast in
-      print_endline @@ L02ast.string_of_value res;
-      flush stdout
+      try
+        let basis = [] in
+        let ast = L01parser.main L00lexer.token lexbuf in
+        let tast = L02ast.typecheck basis ast in
+        let res = L03eval.eval basis tast in
+        (
+          print_endline @@ L02ast.string_of_value res;
+          flush stdout
+        )
+      with Failure _ -> exit 0
       (* let str = L02ast.string_of_aexp Types.string_of_ty tast in
       print_endline str; flush stdout
       *)
