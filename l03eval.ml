2b83a09397db10ea12db3eff2c906f077584dd07
Maxwell Bernstein <tekknolagi@gmail.com>
2017-11-09 21:15:25 -0500

    Match_failure ugh

--- l03eval.ml
+++ l03eval.ml
@@ -1,6 +1,7 @@
 exception BugInTypeChecking of string
 exception DivByZero
 
+[@@@ warning "-8"] (* Ignore non-exhaustive *)
 let rec eval varenv (exp : Types.ty L02ast.exp) =
   let open L02ast in
   let rec ev = function
@@ -11,47 +12,37 @@ let rec eval varenv (exp : Types.ty L02ast.exp) =
     | Var (n, _) -> List.assoc n varenv
     | Plus (e1, e2, _) ->
         (match (ev e1, ev e2) with
-        | (IntVal v1, IntVal v2) -> IntVal (v1+v2)
-        | _ -> raise @@ BugInTypeChecking "Plus")
+        | (IntVal v1, IntVal v2) -> IntVal (v1+v2))
     | Minus (e1, e2, _) ->
         (match (ev e1, ev e2) with
-        | (IntVal v1, IntVal v2) -> IntVal (v1-v2)
-        | _ -> raise @@ BugInTypeChecking "Minus")
+        | (IntVal v1, IntVal v2) -> IntVal (v1-v2))
     | Times (e1, e2, _) ->
         (match (ev e1, ev e2) with
-        | (IntVal v1, IntVal v2) -> IntVal (v1*v2)
-        | _ -> raise @@ BugInTypeChecking "Times")
+        | (IntVal v1, IntVal v2) -> IntVal (v1*v2))
     | Divide (e1, e2, _) ->
         (match (ev e1, ev e2) with
         | (IntVal v1, IntVal 0) -> raise @@ DivByZero
-        | (IntVal v1, IntVal v2) -> IntVal (v1/v2)
-        | _ -> raise @@ BugInTypeChecking "Divide")
+        | (IntVal v1, IntVal v2) -> IntVal (v1/v2))
     | Not (e, _) ->
         (match ev e with
-        | BoolVal v -> BoolVal (not v)
-        | _ -> raise @@ BugInTypeChecking "Not")
+        | BoolVal v -> BoolVal (not v))
     | Lt (e1, e2, _) ->
         (match (ev e1, ev e2) with
-        | (IntVal v1, IntVal v2) -> BoolVal (v1 < v2)
-        | _ -> raise @@ BugInTypeChecking "Lt")
+        | (IntVal v1, IntVal v2) -> BoolVal (v1 < v2))
     | Lte (e1, e2, _) ->
         (match (ev e1, ev e2) with
-        | (IntVal v1, IntVal v2) -> BoolVal (v1 <= v2)
-        | _ -> raise @@ BugInTypeChecking "Lte")
+        | (IntVal v1, IntVal v2) -> BoolVal (v1 <= v2))
     | Gt (e1, e2, _) ->
         (match (ev e1, ev e2) with
-        | (IntVal v1, IntVal v2) -> BoolVal (v1 > v2)
-        | _ -> raise @@ BugInTypeChecking "Gt")
+        | (IntVal v1, IntVal v2) -> BoolVal (v1 > v2))
     | Gte (e1, e2, _) ->
         (match (ev e1, ev e2) with
-        | (IntVal v1, IntVal v2) -> BoolVal (v1 >= v2)
-        | _ -> raise @@ BugInTypeChecking "Gte")
+        | (IntVal v1, IntVal v2) -> BoolVal (v1 >= v2))
     | Equals (e1, e2, _) -> BoolVal ((ev e1)=(ev e2))
     | IfElse (cond, iftrue, iffalse, _) ->
         (match ev cond with
         | BoolVal true -> ev iftrue
-        | BoolVal false -> ev iffalse
-        | _ -> raise @@ BugInTypeChecking "IfElse")
+        | BoolVal false -> ev iffalse)
     | Let ((n, _), e, b, _) ->
         let varenv' = (n, ev e)::varenv in
         eval varenv' b
@@ -62,6 +53,7 @@ let rec eval varenv (exp : Types.ty L02ast.exp) =
         (match ev f with
         | ClosureVal (formals, body) ->
             let varenv' = (List.combine formals actualsv) @ varenv in
-            eval varenv' body
-        | _ -> raise @@ BugInTypeChecking "App")
-  in ev exp
+            eval varenv' body)
+  in
+  try ev exp
+  with Match_failure _ -> raise @@ BugInTypeChecking "Somewhere"
