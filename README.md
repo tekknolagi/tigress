0b818b172e18423a7bb551d8f2f029d8cb4c551c
Maxwell Bernstein <max@bernsteinbear.com>
2017-12-02 02:56:33 -0500

    Add modes to REPL

--- README.md
+++ README.md
@@ -11,6 +11,25 @@ Install `ocaml` (at least 4.02) and `menhir`. Then run `make`.
 ## What does that produce?
 
 Two binaries: `tests.native`, which is run automatically upon build, and
-`main.native`, which contains a REPL. The REPL fluctuates between typechecking,
-compiling, and evaluating code. I should probably put some command-line
-arguments in there.
+`main.native`, which contains a REPL.
+
+The REPL has several different modes which are described in the `mode` type in
+[main.ml](main.ml). You can select one by doing, for example,
+
+```
+oak% rlwrap ./main.native parse
+REPL in parse mode.
+>>> let Fact(X:Int):Int = if X < 2 then 1 else X*Fact(X-1) in Fact(5);
+Let (Fact, Fun ([X : Int], IfElse (Lt (Var X, IntLit 2), IntLit 1, Times (Var X, App (Var Fact, [Minus (Var X, IntLit 1)])))), App (Var Fact, [IntLit 5]))
+>>>
+```
+
+or
+
+```
+oak% rlwrap ./main.native eval
+REPL in eval mode.
+>>> let Fact(X:Int):Int = if X < 2 then 1 else X*Fact(X-1) in Fact(5);
+120
+>>>
+```
--- main.ml
+++ main.ml
@@ -1,20 +1,62 @@
+open Common
+
+exception InvalidMode of string
+
+type mode = Parse | Type | Rename | Eval | Lower | Codegen
+let mode_of_string s =
+  let opts =
+    ["parse",Parse; "type",Type; "rename", Rename; "eval",Eval; "lower",Lower;
+    "codegen", Codegen]
+  in
+  match (L.assoc_opt s opts) with
+  | Some opt -> opt
+  | None -> raise @@ InvalidMode s
+
+let string_of_mode = function
+  | Parse -> "parse" | Type -> "type" | Rename -> "rename" | Eval -> "eval"
+  | Lower -> "lower" | Codegen -> "codegen"
+
 let const (type a) (type b) (x : a) (y : b) : a = x
 
+exception BugInREPL of string
+
 let _ =
+  let default_mode = Lower in
+  let mode =
+    if Array.length Sys.argv < 2
+    then default_mode
+    else mode_of_string Sys.argv.(1)
+  in
+  print_endline @@ "REPL in " ^ string_of_mode mode ^ " mode.";
   try
     let lexbuf = Lexing.from_channel stdin in
     while true do
       try
         if Unix.(isatty stdin) then ( print_string ">>> "; flush stdout );
+
+        let to_print =
         let ast = L01parser.main L00lexer.token lexbuf in
+        if mode=Parse
+        then L02ast.string_of_aexp (const "") ast
+        else
         let tst = L03typecheck.typecheck [] ast in
-        (* print_endline @@ L02ast.string_of_aexp Types.string_of_ty tst; *)
+        if mode=Type
+        then L02ast.string_of_aexp Types.string_of_ty tst
+        else
         let ren = L04scoperesolution.rename [] tst in
-        (* let res = L05eval.eval [] ren in *)
-        let funs = L05mir.lower ren in
-        (
-          print_endline @@ String.concat "\n" @@ List.map L05mir.string_of_funrep funs
-        )
+        if mode=Rename
+        then L02ast.string_of_aexp (const "") ren
+        else
+        let res = L05eval.eval [] ren in
+        if mode=Eval
+        then L02ast.string_of_value res
+        else
+        let low = L05mir.lower ren in
+        if mode=Lower
+        then String.concat "\n" @@ List.map L05mir.string_of_funrep low
+        else raise @@ BugInREPL "Invalid mode"
+        in
+        print_endline to_print
       with
       | Failure _ -> exit 0
       | e -> ( print_endline @@ Printexc.to_string e;
